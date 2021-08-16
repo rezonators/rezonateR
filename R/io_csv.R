32d25f0f10e08553394a47acfc8ab494df8ea998
Ryan Ka Yau Lai <43101723+kayaulai@users.noreply.github.com>
2021-08-16 16:44:56 -0700

    Add import / export CSV functionality

--- R/io_csv.R
+++ R/io_csv.R
@@ -1,5 +1,66 @@
 #A series of functions for handling export to and from CSVs.
 
-rez_to_csv = function(rezrDF){
+#' Read and write rezrDFs as CSV files
+#'
+#' @param df The rezrDF to be exported.
+#' @param path The path from which a CSV is to be imported / exported.
+#' @param inclCols Columns to be included from the import / export.
+#' @param exclCols Columns to be excluded from the import / export.
+#' @param lubridate Are you using the lubridate package?
+#' @param origDF The rezrDF that originally produced this CSV, used to identify data types of the columns.
+#' @param ... Arguments passed onto read_csv and write_csv.
+#'
+#' @return For rez_read_csv, the CSV being imported.
+#' @export
+rez_write_csv = function(df, path, inclCols = character(0), exclCols = character(0), ...){
+  if(length(inclCols) > 0){
+    if(length(exclCols) > 0){
+      warning("You specified columns to both include and exclude. I will ignore the exclude parameter.")
+    }
+  } else {
+    inclCols = setdiff(colnames(df), exclCols)
+  }
 
+  write_csv(df %>% select(all_of(inclCols)), path, ...)
+}
+
+
+rez_read_csv = function(path, origDF = NULL, lubridate = F, inclCols = character(0), exclCols = character(0), ...){
+  inImport = strsplit(read_lines(path, n_max = 1), ",")[[1]]
+  if(length(inclCols) > 0){
+    if(length(exclCols) > 0){
+      warning("You specified columns to both include and exclude. I will ignore the exclude parameter.")
+    }
+    inclCols = intersect(inImport, inclCols)
+  } else {
+    inclCols = setdiff(inImport, exclCols)
+  }
+
+  if(!("col_types" %in% names(list(...))) & !is.null(origDF)){
+    oldNames = colnames(origDF)
+    colTypes = sapply(inclCols, function(x){
+      result = col_guess()
+      if(x %in% oldNames){
+        if(is.character(origDF[[x]])) result = col_character()
+        else if(is.logical(origDF[[x]])) result = col_logical()
+        else if(is.integer(origDF[[x]])) result = col_integer()
+        else if(is.double(origDF[[x]])) result = col_double()
+        else if(is.numeric(origDF[[x]])) result = col_number()
+        else if(is.date(origDF[[x]])) result = col_date()
+        else if(is.time(origDF[[x]])) result = col_time()
+        else if(lubridate){
+          if(lubridate::is.instant(origDF[[x]]) | lubridate::is.timepoint(origDF[[x]])){
+            result = col_datetime()
+          }
+        }
+      }
+      result
+    })
+
+  } else {
+    colTypes = sapply(inclCols, function(x) col_guess())
+  }
+
+  newDF = read_csv(path, col_types = exec("cols_only", !!!colTypes), ...)
+  newDF
 }
--- tests/testthat/test-rezrObj.R
+++ tests/testthat/test-rezrObj.R
@@ -17,3 +17,17 @@ test_that("rezrObj save/load", {
   a = rez_load("inst/extdata/rezEx.Rdata")
   expect_s3_class(a, "rezrObj")
 })
+
+test_that("CSV import/export", {
+  discoName = "three-parting-2569_new"
+  path = "C:/Users/User/Documents/GitHub/lhasa-reference-tracking/shanti/3_2_rez_file/" %+% discoName %+% ".rez"
+  rezEx = importRez(path, layerRegex = list(track = list(field = "name", regex = c("CLAUSEARG_", "DISCDEIX_"), names = c("clausearg", "discdeix", "refexpr")), chunk = list(field = "chunkLayer", regex = c("verb", "adv", "predadj"), names = c("verb", "adv", "predadj", "refexpr"))))
+
+  rez_write_csv(rezEx$tokenDF, "inst/extdata/token.csv")
+  b = rez_read_csv("inst/extdata/chain.csv", inclCols = c("id", "tokenSeq"))
+  expect_type(b$id, "character")
+  expect_type(b$tokenSeq, "numeric")
+  b = rez_read_csv("inst/extdata/chain.csv", a$trackDF, exclCols = c("unit", "tokenSeq"))
+  expect(!("unit" %in% names(b)), "Exclusion failed")
+})
+
